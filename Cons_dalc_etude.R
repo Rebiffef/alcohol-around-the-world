d0e2e5b83d6dc5b83fd6cfcec60056647c6d080a
mckennak <kevin.ryan.mckenna@gmail.com>
2021-01-18 16:28:55 +0100

    reecriture du code pour avoir le gather

--- Cons_dalc_etude.R
+++ Cons_dalc_etude.R
@@ -1,8 +1,8 @@
 library(tidyverse)
-alc18_10 <- read.csv("/home/kevin/M1SSD/Logiciel_Special/TableauAlcoolComparaison/alc18_10.csv")
-alc09_00 <- read.csv("/home/kevin/M1SSD/Logiciel_Special/TableauAlcoolComparaison/alc09_00.csv")
-alc99_80 <- read.csv("/home/kevin/M1SSD/Logiciel_Special/TableauAlcoolComparaison/alc99_80.csv")
-alc79_60 <- read.csv("/home/kevin/M1SSD/Logiciel_Special/TableauAlcoolComparaison/alc79_60.csv")
+alc18_10 <- read.csv("~/8)SSD/LogSpec/alcohol-around-the-world-main/new.alc18_10.csv")
+alc09_00 <- read.csv("~/8)SSD/LogSpec/alcohol-around-the-world-main/new.alc09_00.csv")
+alc99_80 <- read.csv("~/8)SSD/LogSpec/alcohol-around-the-world-main/new.alc99_80.csv")
+alc79_60 <- read.csv("~/8)SSD/LogSpec/alcohol-around-the-world-main/new.alc79_60.csv")
 
 colnames(alc18_10) <- c("Country","Data Source", "Beverage",alc18_10[1,4:length(alc18_10)])
 alc18_10 <- alc18_10[2:nrow(alc18_10),1:length(alc18_10)]
@@ -18,9 +18,6 @@ alc79_60 <- alc79_60[2:nrow(alc79_60),1:length(alc79_60)]
 
 alc18_60 <- full_join(full_join(full_join(alc18_10,alc09_00),alc99_80),alc79_60)
 
-# Doit changer pour qu'ils ont tous le meme DataSource, car il y a Nauru qui 
-# apparait 2 fois dans alc99_80.  Aussi dans Beverage il y a un "Beverage Type" 
-# qu'on doit probablement enlever.  Hmmm non il y a aucun alors peutetre c'est bon. 
-summary(alc18_60)
-str(alc18_60)
-levels(alc18_60[[3]])
+alc18_60 <- gather(alc18_60, key = "year", value ="Alcohol..recorded.per.capita..15...consumption..in.litres.of.pure.alcohol.", -c("Country","Data Source","Beverage"))
+
+write_excel_csv(alc18_60, file="alc_total.csv")
